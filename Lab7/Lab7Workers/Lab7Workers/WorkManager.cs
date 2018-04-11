ae9b4f6b029745fa0455d5939b99321298799658
chudytom <tomasz.chudzik.pl@gmail.com>
2018-04-12 01:21:51 +0200

    Faster version of algorithm working is the other case. Some problems with Stackoveflow

--- Lab7/Lab7Workers/Lab7Workers/WorkManager.cs
+++ Lab7/Lab7Workers/Lab7Workers/WorkManager.cs
@@ -18,8 +18,8 @@ namespace ASD
         /// </summary>
         public int[] DivideWorkersWork(int[] blocks, int expectedBlockSum)
         {
-
-            //bool[] usedBlocks = new bool[blocks.Length];
+            if (blocks == null || blocks.Length == 0)
+                return null;
             int[] resultBlocks = new int[blocks.Length];
             bool divisionPossible = false;
 
@@ -53,11 +53,13 @@ namespace ASD
                 if(newWorkerSum < expectedBlockSum)
                 {
                     int currentIndex = blockIndex + 1;
+                    int previousIndex = -1;
                     while (currentIndex < blocks.Length)
                     {
                         bool isDivision1Possible = giveBlock(blockIndex: currentIndex, workerNumber: workerNumber, workerSum: newWorkerSum);
                         if (isDivision1Possible)
                             return true;
+                        previousIndex = currentIndex;
                         currentIndex++;
                     }
                 }
@@ -71,11 +73,13 @@ namespace ASD
                     else
                     {
                         int currentIndex = 0;
+                        int previousIndex = -1;
                         while (currentIndex < blocks.Length)
                         {
                             bool isDivision2Possible = giveBlock(blockIndex: currentIndex, workerNumber: 2, workerSum: 0);
                             if (isDivision2Possible)
                                 return true;
+                            previousIndex = currentIndex;
                             currentIndex++;
                         }
                     }
@@ -92,113 +96,121 @@ namespace ASD
         /// </summary>
         public int[] DivideWorkWithClosestBlocksCount(int[] blocks, int expectedBlockSum)
         {
-
-            List<int[]> solutions = new List<int[]>();
-            int worker1Sum = 0;
-            int worker2Sum = 0;
-
-            //bool[] usedBlocks = new bool[blocks.Length];
-            int[] resultBlocks = new int[blocks.Length];
-
-            bool result = false;
-            for (int block = 0; block < blocks.Length; block++)
-            {
-                if (GiveBlockTo1(block, 0) == true)
-                {
-                    result = true;
-                }
-            }
-
-            int[] bestSolution = new int[blocks.Length];
-            int bestDifference = int.MaxValue;
-
-            foreach (var solution in solutions)
-            {
-                int worker1 = 0;
-                int worker2 = 0;
-
-                for (int blockIndex = 0; blockIndex < blocks.Length; blockIndex++)
-                {
-                    if (solution[blockIndex] == 1)
-                        worker1++;
-                    if (solution[blockIndex] == 2)
-                        worker2++;
-                }
-                int diff = Math.Abs(worker1 - worker2);
-                if (diff < bestDifference)
-                {
-                    bestDifference = diff;
-                    bestSolution = solution;
-                }
-            }
-
-
-
-            if (solutions.Count == 0)
+            if (blocks == null || blocks.Length == 0)
                 return null;
-            else
-                return bestSolution;
 
+            int currentBestDiff = int.MaxValue;
+            int[] bestSolution = new int[blocks.Length];
+            //List<int[]> solutions = new List<int[]>();
+            int[] resultBlocks = new int[blocks.Length];
+            bool divisionPossible = false;
 
-            bool GiveBlockTo1(int blockIndex, int workerSum)
+            for (int blockIndex = 0; blockIndex < blocks.Length; blockIndex++)
             {
-                resultBlocks[blockIndex] = 1;
-                int currentSum = workerSum + blocks[blockIndex];
-                if (currentSum > expectedBlockSum)
-                {
-                    resultBlocks[blockIndex] = 0;
-                    return false;
-                }
-                if (currentSum == expectedBlockSum)
-                {
-                    for (int block2 = 0; block2 < blocks.Length; block2++)
-                    {
-                        if (resultBlocks[block2] != 0)
-                            continue;
-                        bool resultFrom2 = GiveBlockTo2(block2, 0);
-                        if (resultFrom2 == true) return true;
-                    }
-                }
-                if (currentSum < expectedBlockSum)
-                {
-                    for (int block = 0; block < blocks.Length; block++)
-                    {
-                        if (resultBlocks[block] != 0)
-                            continue;
-                        bool resultFrom1 = GiveBlockTo1(block, currentSum);
-                        if (resultFrom1 == true) return true;
-                    }
-                }
-                resultBlocks[blockIndex] = 0;
-                return false;
-
+                divisionPossible = giveBlock(blockIndex: blockIndex, workerNumber: 1, workerSum: 0);
+                if (divisionPossible)
+                    break;
             }
 
-            bool GiveBlockTo2(int blockIndex, int workerSum)
+            //int[] bestSolution = new int[blocks.Length];
+            //int bestDifference = int.MaxValue;
+
+            //foreach (var solution in solutions)
+            //{
+            //    int worker1 = 0;
+            //    int worker2 = 0;
+
+            //    for (int blockIndex = 0; blockIndex < blocks.Length; blockIndex++)
+            //    {
+            //        if (solution[blockIndex] == 1)
+            //            worker1++;
+            //        if (solution[blockIndex] == 2)
+            //            worker2++;
+            //    }
+            //    int diff = Math.Abs(worker1 - worker2);
+            //    if (diff < bestDifference)
+            //    {
+            //        bestDifference = diff;
+            //        bestSolution = solution;
+            //    }
+            //}
+
+            if (currentBestDiff < int.MaxValue)
+                return bestSolution;
+            else
+                return null;
+
+            bool giveBlock(int blockIndex, int workerNumber, int workerSum)
             {
-                resultBlocks[blockIndex] = 2;
-                int currentSum = workerSum + blocks[blockIndex];
-                if (currentSum > expectedBlockSum)
+                if (blockIndex >= blocks.Length || resultBlocks[blockIndex] != 0)
+                {
+                    return false;
+                }
+                resultBlocks[blockIndex] = workerNumber;
+                int newWorkerSum = workerSum + blocks[blockIndex];
+                if (newWorkerSum > expectedBlockSum)
                 {
                     resultBlocks[blockIndex] = 0;
                     return false;
                 }
-                if (currentSum == expectedBlockSum)
+
+                if (newWorkerSum < expectedBlockSum)
                 {
-                    solutions.Add((int[])resultBlocks.Clone());
-                    resultBlocks[blockIndex] = 0;
-                    return false;
-                }
-                if (currentSum < expectedBlockSum)
-                {
-                    for (int block2Index = 0; block2Index < blocks.Length; block2Index++)
+                    int currentIndex = blockIndex + 1;
+                    while (currentIndex < blocks.Length)
                     {
-                        if (resultBlocks[block2Index] != 0)
-                            continue;
-                        bool resultFrom2 = GiveBlockTo2(block2Index, currentSum);
-                        if (resultFrom2 == true) return true;
+                        bool isDivision1Possible = giveBlock(blockIndex: currentIndex, workerNumber: workerNumber, workerSum: newWorkerSum);
+                        if (isDivision1Possible)
+                            return true;
+                        currentIndex++;
                     }
                 }
+
+                else
+                {
+                    if (workerNumber == 2)
+                    {
+                        
+
+                        int worker1 = 0;
+                        int worker2 = 0;
+
+                        for (int index = 0; index < blocks.Length; index++)
+                        {
+                            if (resultBlocks[index] == 1)
+                                worker1++;
+                            if (resultBlocks[index] == 2)
+                                worker2++;
+                        }
+                        int diff = Math.Abs(worker1 - worker2);
+                        if (diff == 0)
+                        {
+                            bestSolution = (int[])resultBlocks.Clone();
+                            currentBestDiff = diff;
+                            return true;
+                        }
+                        if (diff < currentBestDiff)
+                        {
+                            currentBestDiff = diff;
+                            bestSolution = (int[])resultBlocks.Clone();
+                        }
+
+                        resultBlocks[blockIndex] = 0;
+                        return false;
+                    }
+                    else
+                    {
+                        int currentIndex = 0;
+                        while (currentIndex < blocks.Length)
+                        {
+                            bool isDivision2Possible = giveBlock(blockIndex: currentIndex, workerNumber: 2, workerSum: 0);
+                            if (isDivision2Possible)
+                                return true;
+                            currentIndex++;
+                        }
+                    }
+                }
+
                 resultBlocks[blockIndex] = 0;
                 return false;
             }
